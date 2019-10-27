57ae399edd1072e83d2bd8a425f00c7c1957b8bd
Henrik Jangefelt Nilsson <henke9105@hotmail.com>
2019-10-27 18:04:27 +0100

    updated

--- src/com/HenrikJangefelt/FileUtils.java
+++ src/com/HenrikJangefelt/FileUtils.java
@@ -13,8 +13,11 @@ import java.util.List;
 public class FileUtils {
 
 
-
-    //Read from file...
+    /**
+     * Fetches text from textFile as an arrayList of strings
+     * @param fileName Takes in a filename or filepath
+     * @return Returns the arrayList with text
+     */
     public static  ArrayList<String> readAllLines(String fileName) {
 
         ArrayList<String> allLines = new ArrayList<>();
@@ -31,6 +34,11 @@ public class FileUtils {
     }
 
 
+    /**
+     * Writes text (as an ArrayList of Strings) to text file.
+     * @param lines Takes in an ArrayList of Strings
+     * @param fileName Takes in a filename as a String
+     */
     public static void writeAllLines(ArrayList<String> lines, String fileName) {
 
         if (!canWriteToFile(fileName)) { return; }
@@ -46,9 +54,12 @@ public class FileUtils {
     }
 
 
-
-
-    // Flexibel för generic objekt
+    /**
+     * Save objects to file.
+     * @param fileName Takes in a filename as a String
+     * @param objectList Takes in an ArrayList (generic) of objects
+     * @param <T> Method is generic
+     */
     public static <T extends Object> void writeGenericObjects(String fileName, List<T> objectList){
         ObjectOutputStream objectOutputStream = null;
         FileOutputStream fileOutputStream = null;
@@ -64,37 +75,12 @@ public class FileUtils {
     }
 
 
-
-
-    // TEST
-    public static <T extends Object> void saveObjects(String filename, ArrayList<T> objects, StandardOpenOption... option) {
-        Path path = Paths.get(filename);
-        try (ObjectOutputStream out = new ObjectOutputStream(Files.newOutputStream(path, option))) {
-            for (T object : objects) {
-                out.writeObject(object);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-
-    // TEST
-    public static <T extends Object> ArrayList<T> loadObjects(String filename) {
-
-        Path path = Paths.get(filename);
-        ArrayList<T> objectList = null;
-        try (ObjectInputStream in = new ObjectInputStream(Files.newInputStream(path))) {
-            return (ArrayList<T>) in.readObject();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
-
-    // TODO: Spara antingen workouts och friends för sig elle gymMember object
+    /**
+     * Fetches objects from file.
+     * @param fileName Takes in a filename to fetch from
+     * @param <T> Is generic
+     * @return Returns the ArrayList of fetched objects
+     */
     public static <T extends Object> ArrayList<T> readGenericObjects(String fileName){
         ObjectInputStream objectinputstream = null;
         ArrayList<T> objectList = null;
@@ -109,20 +95,52 @@ public class FileUtils {
         return objectList;
     }
 
-
-
-
-
+    /**
+     * Set file as read only
+     * @param fileName
+     */
     public static void setReadOnly(String fileName) {
         File file = new File(fileName);
         file.setReadOnly();
     }
 
-
+    /**
+     * Check if file can be written to.
+     * @param fileName
+     * @return
+     */
     public static boolean canWriteToFile(String fileName) {
         File file = new File(fileName);
         return file.canWrite();
     }
+
+
+
+    // TEST
+    /*public static <T extends Object> void saveObjects(String filename, ArrayList<T> objects, StandardOpenOption... option) {
+        Path path = Paths.get(filename);
+        try (ObjectOutputStream out = new ObjectOutputStream(Files.newOutputStream(path, option))) {
+            for (T object : objects) {
+                out.writeObject(object);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }*/
+
+
+    // TEST
+    /*public static <T extends Object> ArrayList<T> loadObjects(String filename) {
+
+        Path path = Paths.get(filename);
+        ArrayList<T> objectList = null;
+        try (ObjectInputStream in = new ObjectInputStream(Files.newInputStream(path))) {
+            return (ArrayList<T>) in.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }*/
 
 }
 
@@ -130,37 +148,6 @@ public class FileUtils {
 
 
 
-
-
-
-    /*public static <T extends Object> ArrayList<T> readObjects(String fileName){
-        ObjectInputStream objectinputstream = null;
-        ArrayList<T> objectList = null;
-        try {
-            FileInputStream streamIn = new FileInputStream(fileName);
-            objectinputstream = new ObjectInputStream(streamIn);
-            objectList = (ArrayList<T>) objectinputstream.readObject();
-            objectinputstream .close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return objectList;
-    }
-
-
-    // Flexibel för generic objekt
-    public static <T extends Object> void writeGenericObjects(String fileName, ArrayList<T> objectList){
-        ObjectOutputStream objectOutputStream = null;
-        FileOutputStream fileOutputStream = null;
-        try{
-            fileOutputStream = new FileOutputStream(fileName, true); // objekt oftast '.ser' true innebär lägger till/false skriver över
-            objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            objectOutputStream.writeObject(objectList);
-            objectOutputStream.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }*/
 
 
 
